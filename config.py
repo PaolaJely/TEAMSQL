af7978226a3fa2838cc2013fb227b3ed8d03bb11
Angie Paola <144299292+PaolaJely@users.noreply.github.com>
2025-06-09 23:48:03 -0600

    REQUERIMIENTOS, RUN Y CONFIG

---
+++ config.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+
+if not SECRET_KEY:
+    print("⚠️ ERROR: SECRET_KEY no cargada desde .env")
+if not OPENAI_API_KEY:
+    print("⚠️ ERROR: OPENAI_API_KEY no cargada desde .env")
