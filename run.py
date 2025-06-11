28b6119e1c01c7c7dc160d5cc1f38d68b71cc9aa
Angie Paola <144299292+PaolaJely@users.noreply.github.com>
2025-06-10 18:39:25 -0600

    Update run para flask

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
