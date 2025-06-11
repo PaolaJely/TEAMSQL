87a959e4e8867e9508d4a9ca48b28b3b82fb4bcf
Angie Paola <144299292+PaolaJely@users.noreply.github.com>
2025-06-10 20:12:12 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-from app import create_app
-
-app = create_app()
+from app import app  # Importa tu aplicación Flask desde la carpeta app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    # Esto SOLO se ejecutará si inicias con python run.py (LOCAL)
+    app.run(host='0.0.0.0', port=5000)
