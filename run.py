0af5329bc58fe29305fcc7b1aac8a5e2053c8d96
Angie Paola <144299292+PaolaJely@users.noreply.github.com>
2025-06-10 23:28:40 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,8 @@
+from app import create_app
 import os
-try:
-    from app import create_app
-except Exception as e:
-    print("ERROR IMPORTANDO create_app():", e)
 
-try:
-    app = create_app()
-except Exception as e:
-    print("ERROR EJECUTANDO create_app():", e)
+app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
