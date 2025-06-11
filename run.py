e9db220c1c6db47b940ffab2c6ca35a0bb5c9fa6
Angie Paola <144299292+PaolaJely@users.noreply.github.com>
2025-06-10 22:51:07 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,3 @@
 import os
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
-
+port = int(os.environ.get("PORT", 5000))  # <-- Usa el puerto que Render asigna
+app.run(host="0.0.0.0", port=port)
